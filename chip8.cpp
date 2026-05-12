fa506f2212b20982c8598d87bfd248d639930fbd
KlonAnon <klonanon.com@gmail.com>
2026-05-12 12:03:22 +0200

    Implemented all opcodes

--- chip8.cpp
+++ chip8.cpp
@@ -20,6 +20,11 @@ void Chip8::reset()
     }
     for (int i = 0; i < 64 * 32; i++)
         display[i] = false;
+
+    for (int i = 0; i < 80; i++)
+    {
+        memory[FONT_START + i] = FONTSET[i];
+    }
 }
 
 bool Chip8::loadRom(const char *filename)
@@ -130,6 +135,7 @@ void Chip8::cycle()
         case 0x6:
             V[0xF] = V[x] & 0x1;
             V[x] >>= 1;
+            break;
         case 0x7:
             V[0xF] = (V[y] > V[x]) ? 1 : 0;
             V[x] = V[y] - V[x];
@@ -151,6 +157,103 @@ void Chip8::cycle()
         pc = nnn + V[0];
         break;
     case 0xC000:
+    {
         uint8_t r = static_cast<uint8_t>(std::rand() % 256);
+        V[x] = r & kk;
+        break;
+    }
+    case 0xD000:
+    {
+        uint8_t n = opcode & 0x000F;
+        for (int row = 0; row < n; row++)
+        {
+            uint8_t spriteByte = memory[I + row];
+            for (int bit = 0; bit < 8; bit++)
+            {
+                uint8_t mask = 0x80 >> bit;
+
+                if (spriteByte & mask)
+                {
+                    int xPos = (V[x] + bit) % 64;
+                    int yPos = (V[y] + row) % 32;
+                    int index = yPos * 64 + xPos;
+
+                    if (display[index])
+                        V[0xF] = 1;
+
+                    display[index] = !display[index];
+                }
+            }
+        }
+        break;
+    }
+    case 0xE000:
+        switch (kk)
+        {
+        case 0x9E:
+            if (keys[V[x]])
+                pc += 4;
+            break;
+        case 0xA1:
+            if (!keys[V[x]])
+                pc += 4;
+            break;
+        }
+        break;
+    case 0xF000:
+        switch (kk)
+        {
+        case 0x07:
+            V[x] = delayTimer;
+            break;
+        case 0x0A:
+        {
+            bool keyPressed = false;
+            for (int k = 0; k < 16; k++)
+            {
+                if (keys[k])
+                {
+                    V[x] = k;
+                    keyPressed = true;
+                    break;
+                }
+            }
+            if (!keyPressed)
+                pc -= 2;
+            break;
+        }
+        case 0x15:
+            delayTimer = V[x];
+            break;
+        case 0x18:
+            soundTimer = V[x];
+            break;
+        case 0x1E:
+            I += V[x];
+            break;
+        case 0x29:
+        {
+            uint8_t digit = V[x] & 0x0F;
+            I = FONT_START + digit * 5;
+            break;
+        }
+        case 0x33:
+            memory[I] = V[x] / 100;
+            memory[I + 1] = (V[x] / 10) % 10;
+            memory[I + 2] = V[x] % 10;
+            break;
+        case 0x55:
+            for (int i = 0; i <= x; i++)
+            {
+                memory[I + i] = V[i];
+            }
+            break;
+        case 0x65:
+            for (int i = 0; i <= x; i++)
+            {
+                V[i] = memory[I + i];
+            }
+            break;
+        }
     }
 }
